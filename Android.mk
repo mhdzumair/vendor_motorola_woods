0c77b228c2f59dc52e004161077502c0b08079c9
mhdzumair <mhdzumair@gmail.com>
2020-02-15 00:33:22 +0530

    merge:update blobs from nicklaus
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter woods, $(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
 LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
